6c853b9864bc3276d83bbcc9d5ce9ba77146face
fizu <farzanehnooribmh@gmail.com>
2024-05-28 14:10:55 +0330

    first commit

---
+++ src/session2.java
@@ -0,0 +1,11 @@
+public class session2 {
+    public static void main(String[] args) {
+        String firstname = "Farzaneh";
+        String lastname = "Noori";
+        String gender = "female";
+        int age = 33;
+        double foot = 40.5;
+        float height = 169.5F;
+
+    }
+}
